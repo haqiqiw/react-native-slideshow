34991ff627024148c43f368a800a46a4d7e1611e
Gerhard Sletten <gerhardsletten@gmail.com>
2017-09-04 13:32:37 +0200

    Should be posible to override the width if the slideshow is not shown in full width of screen

--- Slideshow.js
+++ Slideshow.js
@@ -41,8 +41,7 @@ const styles = StyleSheet.create({
     opacity: 1,
   },
   containerImage : {
-    flex: 1,
-    width: Dimensions.get('window').width,
+    flex: 1
   },
   overlay: {
     opacity: 0.5,
@@ -78,9 +77,14 @@ export default class Slideshow extends Component {
     this.state = {
       position: 0,
       height: Dimensions.get('window').width * (4 / 9),
-      width: Dimensions.get('window').width,
+      width: props.width || Dimensions.get('window').width,
       scrolling: false,
     };
+  }
+
+  containerImageStyle() {
+    const style = styles.containerImage
+    style.width = this.props.width || Dimensions.get('window').width
   }
 
   _onRef(ref) {
@@ -130,7 +134,7 @@ export default class Slideshow extends Component {
   }
 
   componentWillMount() {
-    const width = this.state.width;
+    const width = this.props.width || this.state.width;
 
     let release = (e, gestureState) => {
       const width = this.state.width;
@@ -156,13 +160,14 @@ export default class Slideshow extends Component {
     this._panResponder = PanResponder.create({
       onPanResponderRelease: release
     });
-
-    this._interval = setInterval(() => {
-      const newWidth = Dimensions.get('window').width;
-      if (newWidth !== this.state.width) {
-        this.setState({width: newWidth});
-      }
-    }, 16);
+    if (!this.props.width) {
+      this._interval = setInterval(() => {
+        const newWidth = Dimensions.get('window').width;
+        if (newWidth !== this.state.width) {
+          this.setState({width: newWidth});
+        }
+      }, 16);
+    }
   }
 
   componentWillUnmount() {
@@ -170,7 +175,7 @@ export default class Slideshow extends Component {
   }
 
   render() {
-    const width = this.state.width;
+    const width = this.props.width || this.state.width;
     const height = this.props.height || this.state.height;
     const position = this._getPosition();
     return (
@@ -207,7 +212,7 @@ export default class Slideshow extends Component {
               </View>
             );
             const imageComponentWithOverlay = (
-              <View key={index} style={styles.containerImage}>
+              <View key={index} style={this.containerImageStyle()}>
                 <View style={styles.overlay}>
                   <Image
                     source={imageObject}
@@ -326,6 +331,7 @@ Slideshow.propTypes = {
 	indicatorSelectedColor: PropTypes.string,
 	height: PropTypes.number,
 	position: PropTypes.number,
+  width: PropTypes.number,
   scrollEnabled: PropTypes.bool,
   containerStyle: PropTypes.object,
   overlay: PropTypes.bool,
